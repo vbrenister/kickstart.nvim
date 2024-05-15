89a9461779ee00ebcfa48ca4752799ade052b53f
Victor Brenister <victorbrenister@vandebron.nl>
2024-05-15 20:27:07 +0300

    Small tweaks

--- lua/kickstart/lazy.lua
+++ lua/kickstart/lazy.lua
@@ -1,5 +1,3 @@
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -7,12 +5,11 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- require('lazy').setup 'kickstart.plugins'
-
 require('lazy').setup('kickstart.plugins', {
+  change_detection = {
+    notify = false,
+  },
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
