5ad39d9c75c86c4913cfdc2a455b7b6e413882f6
Victor Brenister <victorbrenister@vandebron.nl>
2024-05-17 10:14:57 +0300

    Setup debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -2,66 +2,50 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     'rcarriga/nvim-dap-ui',
-
     'nvim-neotest/nvim-nio',
-
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
     'leoluz/nvim-dap-go',
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap, dapui = require 'dap', require 'dapui'
 
-    require('mason-nvim-dap').setup {
-      automatic_installation = true,
+    require('dapui').setup()
+    require('dap-go').setup()
 
-      handlers = {},
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-      ensure_installed = {
-        'delve',
+    dap.configurations.scala = {
+      {
+        type = 'scala',
+        request = 'launch',
+        name = 'RunOrTest',
+        metals = {
+          runType = 'runOrTestFile',
+        },
       },
-    }
-
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<leader>Dc', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<leader>Di', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>Dv', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<leader>Do', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
-
-    dapui.setup {
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+      {
+        type = 'scala',
+        request = 'launch',
+        name = 'Test Target',
+        metals = {
+          runType = 'testTarget',
         },
       },
     }
 
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<leader>Dt', dapui.toggle, { desc = 'Debug: See last session result.' })
+    local map = vim.keymap.set
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    require('dap-go').setup {
-      delve = {
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    map('n', '<leader>dc', dap.continue, { desc = 'Debug Continue' })
+    map('n', '<leader>dt', dap.toggle_breakpoint, { desc = 'Debug Toggle Breakpoint' })
   end,
 }
