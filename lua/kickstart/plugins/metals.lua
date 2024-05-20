1d6c40fb6a0f53077a76c2076c198ad4576106bf
Victor Brenister <victorbrenister@vandebron.nl>
2024-05-20 22:50:20 +0300

    Fixed metals multiproject setup

--- lua/kickstart/plugins/metals.lua
+++ lua/kickstart/plugins/metals.lua
@@ -15,16 +15,19 @@ return {
     metals_config.settings = {
       showImplicitArguments = true,
     }
+
     metals_config.init_options.statusBarProvider = 'off'
     metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
     metals_config.on_attach = function()
       require('metals').setup_dap()
     end
 
+    metals_config.find_root_dir_max_project_nesting = 5
+
     return metals_config
   end,
   config = function(self, metals_config)
-    local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+    local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = false })
     vim.api.nvim_create_autocmd('FileType', {
       pattern = self.ft,
       callback = function()
