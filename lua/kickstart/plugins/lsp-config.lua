ee907431dad857d8473d13c19f3eef6b3663b181
Victor Brenister <victorbrenister@vandebron.nl>
2024-05-21 00:47:39 +0300

    Added java config

--- lua/kickstart/plugins/lsp-config.lua
+++ lua/kickstart/plugins/lsp-config.lua
@@ -93,6 +93,9 @@ return {
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+    require('java').setup()
+    require('lspconfig').jdtls.setup {}
+
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
